6dfbacdc3401e23087731743b2a612c79b907d5c
Stephanos Bacon <stephanosbacon@gmail.com>
2017-02-09 11:52:25 -0500

    fix typo

--- config/config-dev.js
+++ config/config-dev.js
@@ -7,7 +7,7 @@ const protocol = process.env.PROTOCOL || 'https';
 const port = normalizePort(process.env.PORT || '3000');
 
 const mongoHost = (process.env.MONGO_SERVICE_HOST || 'localhost');
-const mongoPort = (process.eng.MONGO_SERVICE_PORT || '27017');
+const mongoPort = (process.env.MONGO_SERVICE_PORT || '27017');
 const databaseUrl = 'mongodb://' + mongoHost + ':' + mongoPort + '/chatter';
 
 const jwtSecret = (process.env.JWT_SECRET || include('keys/secrets.js')
