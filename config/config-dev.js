2a8ec7c084eb15ce5ea180e5a729cb654ff3c056
Stephanos Bacon <stephanosbacon@gmail.com>
2017-02-11 12:52:01 -0500

    clean up config a bit

--- config/config-dev.js
+++ config/config-dev.js
@@ -3,19 +3,19 @@
 const fs = require('fs');
 const normalizePort = include('config/normalizePort');
 
-const protocol = process.env.PROTOCOL || 'https';
+const protocol = process.env.PROTOCOL || 'http';
 const port = normalizePort(process.env.PORT || '3000');
-
 const mongoHost = (process.env.MONGO_SERVICE_HOST || 'localhost');
 const mongoPort = (process.env.MONGO_SERVICE_PORT || '27017');
+const certsPath = (process.env.CERTS_PATH);
+const jwtSecret = (process.env.JWT_SECRET ||
+  'YOU MUST SET THE JWT_SECRET ENVIRONMENT VARIABLE (use secret in openshift)');
+
 const databaseUrl = 'mongodb://' + mongoHost + ':' + mongoPort + '/chatter';
 
-const jwtSecret = (process.env.JWT_SECRET || include('keys/secrets.js')
-  .jwtSecret);
-
 const httpOptions = protocol === 'https' ? {
-  key: fs.readFileSync('./keys/server.key'),
-  cert: fs.readFileSync('./keys/server.crt')
+  key: fs.readFileSync(certsPath + '/server.key'),
+  cert: fs.readFileSync(certsPath + '/server.crt')
 } : null;
 
 module.exports = {
