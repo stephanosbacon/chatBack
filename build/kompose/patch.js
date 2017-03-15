48099a6091586cb7450f03c2d9dd1fa1ffb1d40c
Stephanos Bacon <stephanosbacon@gmail.com>
2017-03-15 17:39:59 -0400

    Got kompose working

---
+++ build/kompose/patch.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let Yaml = require('yamljs');
+let fs = require('fs');
+
+function writeObj(obj, file) {
+  let str = Yaml.stringify(obj, 20, 2);
+  fs.writeFile(file, str, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
+
+
+// Workaround for https://github.com/kubernetes-incubator/kompose/issues/486
+// Note that a dockerFilePath of the form "./whatever" gets ignored
+let buildConfig = Yaml.load('./converted/chatback-buildconfig.yaml');
+buildConfig.spec.strategy
+  .dockerStrategy.dockerFilePath = 'build/Dockerfiles/Dockerfile_server';
+writeObj(buildConfig, './converted/chatback-buildconfig.yaml');
+
+
+// Edit the buildconfig for chatback so it uses the jwtsecret secret
+//
+let dc = Yaml.load('./converted/chatback-deploymentconfig.yaml');
+let env = dc.spec.template.spec.containers[0].env;
+let secret = env.find((elt) => {
+  return elt.name === 'JWT_SECRET';
+});
+delete secret.value;
+secret.valueFrom = {
+  'secretKeyRef': {
+    'name': 'jwtsecret',
+    'key': 'jwtsecret'
+  }
+};
+writeObj(dc, './converted/chatback-deploymentconfig.yaml');
